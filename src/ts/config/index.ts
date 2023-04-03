753143191d5b3fed957b011ff6dfa3fbd4b10bb2
John Soto <soto.ja415@gmail.com>
2023-04-03 13:42:31 -0400

    Attempt to fix api error when deployed

--- src/ts/config/index.ts
+++ src/ts/config/index.ts
@@ -6,8 +6,11 @@
 //   FORM_CLOSE_SEC: 2500,
 // };
 
-export const API_URL = import.meta.env.VITE_FORKIFY_API_URL;
-export const API_KEY = import.meta.env.VITE_FORKIFY_API_KEY;
+// export const API_URL = import.meta.env.VITE_FORKIFY_API_URL;
+// export const API_KEY = import.meta.env.VITE_FORKIFY_API_KEY;
+
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const API_KEY = "2815b3a7-2c83-43a4-a400-de9c7cc2e850";
 export const TIME_IN_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const FORM_CLOSE_SEC = 2.5;
